d1742094011156e1543c25b99b7e677424df06bb
felikeys <felixnkrumah567@gmail.comgit config --global user.email felixnkrumah567@gmail.com>
2022-07-06 21:35:23 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ *_abs - Check Holberton
+ * @num: An integre input
+ * Description: This function computes absolute value of a number
+ * Return: Absolute value of number num
+ */
+
+int _abs(int num)
+
+{
+
+	if (num < 0)
+	{	
+		return (num * -1);
+	}
+	else
+	{
+		return (num);
+
+	}
+
+
+
+}
