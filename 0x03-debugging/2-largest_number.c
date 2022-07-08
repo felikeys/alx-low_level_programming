72f7e72297eafd5e958e6a48cf2b87897633669a
felikeys <felixnkrumah567@gmail.comgit config --global user.email felixnkrumah567@gmail.com>
2022-07-08 05:26:50 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,11 +18,7 @@ int largest_number(int a, int b, int c)
 		largest = a;
 
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (c > b)
+	else if (c > b && b > a)
 	{
 		largest = c;
 	}
