29ce48420c79900b2e1c1e4c1229e91768380706
felikeys <felixnkrumah567@gmail.comgit config --global user.email felixnkrumah567@gmail.com>
2022-07-12 15:36:23 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - function takes a point to an int as a parameter and
+*updates the value to 98
+*
+* @n: An input character
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
