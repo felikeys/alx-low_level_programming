79a58132da59b7e22dd5697bd74a73773efe173d
felikeys <felixnkrumah567@gmail.comgit config --global user.email felixnkrumah567@gmail.com>
2022-07-15 04:13:07 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,19 +10,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int l;
+	int m = 0;
 
-	p = a;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < (l / 2); i++)
 	{
-		p++;
-	}
-
-	for (int k = 0; k < i / 2; i++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		m = a[i];
+		a[i] = a[l - i - 1];
 	}
 }
